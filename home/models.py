c897d3d4b102d4ef13fa92540f7e0f272dbc85a5
Bob Dattore <dattore@ucar.edu>
2025-03-18 14:34:37 -0600

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
@@ -5,5 +6,6 @@ from wagtail.core.models import Page
 class HomePage(Page):
     body = RichTextField(blank=False, default="")
     content_panels = Page.content_panels + [
+        FieldPanel("welcome", classname="collapsible collapsed"),
     ]
     is_creatable = False
