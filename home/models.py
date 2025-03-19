8c93c5fe5770a9f158afe9d42702ddfddf48ab45
Bob Dattore <dattore@ucar.edu>
2025-03-19 13:08:19 -0600

    Update models.py

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from wagtail.admin.panels import (
     PageChooserPanel
 )
 from wagtail.fields import RichTextField
-from wagtail.images.panels import ImageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.models import Page
 from wagtail.snippets.models import register_snippet
 
