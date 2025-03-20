31f3cdfc84dc6510e0ca8353157534d9a48701a6
rda-dattore <dattore@ucar.edu>
2025-03-19 21:11:56 -0600

    initial commit

---
+++ mywebserver/settings/navigation_bar.py
@@ -0,0 +1,89 @@
+NAVIGATION_BAR = {
+    'menus': [
+        {
+            'title': 'Datasets',
+            'url': '/gsearch/dataset-search/',
+            'megamenu': [
+                {
+                    'label': 'GCMD Topic',
+                    'links': [
+                        {
+                            'title': 'Agriculture',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Agriculture',
+                        },
+                        {
+                            'title': 'Atmosphere',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Atmosphere',
+                        },
+                        {
+                            'title': 'Biosphere',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Biosphere',
+                        },
+                        {
+                            'title': 'Climate Indicators',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Climate%20Indicators',
+                        },
+                        {
+                            'title': 'Cryosphere',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Cryosphere',
+                        },
+                        {
+                            'title': 'Human Dimensions',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Human%20Dimensions',
+                        },
+                        {
+                            'title': 'Land Surface',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Land%20Surface',
+                        },
+                        {
+                            'title': 'Oceans',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Oceans',
+                        },
+                        {
+                            'title': 'Paleoclimate',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Paleoclimate',
+                        },
+                        {
+                            'title': 'Solid Earth',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Solid%20Earth',
+                        },
+                        {
+                            'title': 'Spectral/engineering',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Spectral/engineering',
+                        },
+                        {
+                            'title': 'Sun-earth Interactions',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Sun-earth%20Interactions',
+                        },
+                        {
+                            'title': 'Terrestrial Hydrosphere',
+                            'url': '/lookfordata/datasets/?nb=y&b=topic&v=Terrestrial%20Hydrosphere',
+                        },
+                    ],
+                },
+                {
+                    'label': 'Atmospheric Reanalysis Data',
+                    'links': [
+                        {
+                            'title': 'All Reanalysis Datasets',
+                            'url': '/lookfordata/datasets/?nb=y&b=plat&v=REANALYSIS%20MODELS',
+                        },
+                    ],
+                },
+                {
+                    'label': 'Station Observations',
+                    'links': [
+                        {
+                            'title': 'Hourly Land Surface Air Temperature',
+                            'url': '/lookfordata/datasets/?b=var&v=Surface%20Air%20Temperature&b=type&v=Platform%20Observation&b=plat&v=GROUND%20STATIONS&b=tres&v=Hourly%20to%20%3C%20Daily',
+                        },
+                        {
+                            'title': 'Monthly Land Surface Air Temperature',
+                            'url': '/lookfordata/datasets/?b=var&v=Surface%20Air%20Temperature&b=type&v=Platform%20Observation&b=plat&v=GROUND%20STATIONS&b=tres&v=Monthly%20to%20%3C%20Annual',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
